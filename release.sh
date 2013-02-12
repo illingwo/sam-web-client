f5168485aeaba46b22d6699a8489a1ce6392b376
Robert Illingworth <illingwo@fnal.gov>
2013-02-12 11:48:44 -0600

    Release script checks if upd is available, and always trys to push tags to the remote repository

--- release.sh
+++ release.sh
@@ -2,7 +2,14 @@
 
 version=${1:?Need to specify release version}
 
+# check upd is available
+
+type -t upd || { echo "upd is not available"; exit 1; }
+
 git tag -a -m "Release $version" $version || exit 1
 make dist || exit 1
 
 upd addproduct sam_web_client -T dist.tar.gz -m ups/sam_web_client.table -0 $version
+
+# Try to push tags to main repository
+git push --tags
