e19dc9132711a79193d30e6d17c9f2fbb16380aa
Robert Illingworth <illingwo@fnal.gov>
2013-04-16 18:57:44 -0500

    Add flag to samweb.serverInfo() method to force secure connection

--- python/samweb_client/misc.py
+++ python/samweb_client/misc.py
@@ -5,8 +5,14 @@ from samweb_client.http_client import escape_url_path
 from samweb_client.exceptions import *
 
 @samweb_method
-def serverInfo(samweb):
-    response = samweb.getURL('')
+def serverInfo(samweb, authenticate=False):
+    """ Get information about the server.
+    arguments:
+        authenticate : force SSL connection. (default False)
+    """
+    kwargs = {}
+    if authenticate: kwargs['secure'] = True
+    response = samweb.getURL('', **kwargs)
     if 'json' in response.headers.get('Content-Type',''):
         return response.json()
     else:
