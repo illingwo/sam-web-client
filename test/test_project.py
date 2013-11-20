5996b83003ec92c3b06f3b438ad1545bd0a478ea
Robert Illingworth <illingwo@fnal.gov>
2013-11-20 11:25:33 -0600

    Add test that actually runs a project

--- test/test_project.py
+++ test/test_project.py
@@ -26,5 +26,47 @@ class TestProjectCommands(testbase.SAMWebCmdTest):
         self.check_cmd_return(cmdline.split())
         assert self.stdout
 
+    def test_project(self):
+        import time, socket, os
+        defname = "run_798_raw_2"
+        projectname = '%s_samweb_test_%s_%s_%s' % (os.getlogin(), socket.gethostname(), os.getpid(), time.time())
+        cmdline = '-e minerva/dev start-project --defname=%s %s' % (defname, projectname)
+        self.check_cmd_return(cmdline.split())
+        projecturl = self.stdout.strip()
+        assert projecturl.startswith('http')
+
+        cmdline = '-e minerva/dev start-process --appname=demo --appversion=1 %s' % projecturl
+        self.check_cmd_return(cmdline.split())
+        processid = int(self.stdout.strip())
+
+        cmdline = '-e minerva/dev get-next-file %s %s' % (projecturl, processid)
+        self.check_cmd_return(cmdline.split())
+        fileurl = self.stdout.strip()
+        assert fileurl
+
+        cmdline = '-e minerva/dev release-file %s %s %s' % (projecturl, processid, fileurl)
+        self.check_cmd_return(cmdline.split())
+
+        cmdline = '-e minerva/dev stop-process %s %s' % (projecturl, processid)
+        self.check_cmd_return(cmdline.split())
+
+        cmdline = '-e minerva/dev stop-project %s' % (projectname)
+        self.check_cmd_return(cmdline.split())
+
+        cmdline = '-e minerva/dev project-summary %s' % (projectname)
+        self.check_cmd_return(cmdline.split())
+        assert self.stdout
+
+    def test_project_with_snapshot(self):
+        import time, socket, os
+        snapshot_id = 7389
+        projectname = '%s_samweb_test_%s_%s_%s' % (os.getlogin(), socket.gethostname(), os.getpid(), time.time())
+        cmdline = '-e minerva/dev start-project --snapshot_id=%s %s' % (snapshot_id, projectname)
+        self.check_cmd_return(cmdline.split())
+        assert self.stdout.startswith('http')
+        cmdline = '-e minerva/dev stop-project %s' % (projectname)
+        self.check_cmd_return(cmdline.split())
+
+
 if __name__ == '__main__':
     unittest.main()
