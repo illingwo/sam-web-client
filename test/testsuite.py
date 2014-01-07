54caf98d580c4734324401e9dff24d5da230c8d1
Robert Illingworth <illingwo@fnal.gov>
2014-01-07 13:03:11 -0600

    Add -v flag to test/testsuite.py

--- test/testsuite.py
+++ test/testsuite.py
@@ -1,13 +1,24 @@
 #! /usr/bin/env python
 
 import unittest
+import optparse
 
 testmodules = ['test_files', 'test_metadata', 'test_admin', 'test_definition', 'test_project']
 
 if __name__ == '__main__':
+    parser = optparse.OptionParser()
+    parser.add_option("-v","--verbose", action="store_true", dest="verbose", default=False,
+            help="Increase verbosity of output")
+    (options, args) = parser.parse_args()
+
     suite = unittest.TestSuite()
     for testmodule in testmodules:
         mod = __import__(testmodule)
         suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(mod))
 
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    if options.verbose:
+        verbosity=2
+    else:
+        verbosity=1
+
+    unittest.TextTestRunner(verbosity=verbosity).run(suite)
